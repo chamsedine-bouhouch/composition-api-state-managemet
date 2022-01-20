b9bf6b45a009f10b1a41be360373516818e9c354
BOUHOUCH chamseddine <48351587+chamsedine-bouhouch@users.noreply.github.com>
2022-01-20 12:50:18 +0100

    fix counter module

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,20 +1,23 @@
-
-import { GET_COUNTER, INCREASE_COUNTER, DECREASE_COUNTER } from '@/store/mutation-types'
+import { reactive } from "vue";
+import {
+  GET_COUNTER,
+  INCREASE_COUNTER,
+  DECREASE_COUNTER
+} from "@/store/mutation-types";
 export default {
-    namespaces: true,
-    state:  {
-        counter: 0
-    },
-    getters: {
-        [GET_COUNTER]: state => state.counter
-    },
-    mutations: {
-        [INCREASE_COUNTER]: state => state.counter++,
-        [DECREASE_COUNTER]: state => state.counter--
-    },
-    actions: {
-        INCREASE_COUNTER: context => context.commit(INCREASE_COUNTER),
-        DECREASE_COUNTER: context => context.commit(DECREASE_COUNTER)
-
-    },
-}
+  namespaces: true,
+  state: reactive({
+    counter: 0
+  }),
+  getters: {
+    [GET_COUNTER]: (state) => state.counter
+  },
+  mutations: {
+    [INCREASE_COUNTER]: (state) => state.counter++,
+    [DECREASE_COUNTER]: (state) => state.counter--
+  },
+  actions: {
+    INCREASE_COUNTER: (context) => context.commit(INCREASE_COUNTER),
+    DECREASE_COUNTER: (context) => context.commit(DECREASE_COUNTER)
+  }
+};
